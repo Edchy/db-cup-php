93d88e97d3ef864f8f81f27ec52fc65601c8e110
Edvard Söderblom <123310716+Edchy@users.noreply.github.com>
2025-05-17 15:40:01 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 
 // $connection = mysqli_connect('localhost', 'edd', 'edd', 'db-cup');
-$connection = mysqli_connect('localhost', 'edso0001', 'Airforce1hv', 'edso0001');
+$connection = mysqli_connect('localhost', 'so0001', 'rce1hv', 'so0001');
 
 if (!$connection) {
   $stat = 'Connection error: ' . mysqli_connect_error();
